9e47c143e742fe02e2459adee1c6bc373e01eb61
mohsinraz3 <mohsinraz3@gmail.com>
2022-10-18 20:12:26 +0500

    node projects

---
+++ myUserInputProgram/index.mjs
@@ -0,0 +1,7 @@
+import PromptSync from "prompt-sync";
+
+const prompt = PromptSync();
+
+let name = Number(prompt(`Whats your name bro? : `))
+
+console.log(name + 4);
---
+++ project0/addTwo.mjs
@@ -0,0 +1,7 @@
+//function add in NodeJS
+
+function addTwo(num){
+    return num + 9;
+}
+
+export {addTwo}
