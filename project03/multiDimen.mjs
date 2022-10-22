d12250256f6ff6f47f37ec8f9228f83d9f2c2466
mohsinraz3 <mohsinraz3@gmail.com>
2022-10-22 23:49:09 +0500

    chapter#3 project completed

--- project03/multiDimen.mjs
+++ project03/multiDimen.mjs
@@ -38,31 +38,120 @@ lang["color"] = "purple";
 
 //updating object
 lang.devLang = false;
-console.log(lang)
+// console.log(lang)
 
 
 //===========> PRACTICE EXERCISE# 3.4 <================//
 
-let myCar = {
+let myCar = [{
     carName: "Mehru",
     noPlate: "azu898",
     model: 1998,
+    forsale: false,
+    colorM: "white",
+    seats: 4,
+    diggi: true
+},
+{
+    carName: "civic",
+    noPlate: "azu898",
+    model: 1998,
+    forsale: false,
     colorM: "white",
     seats: 4,
     diggi: true
 }
+]
+
+let variable = "carName";
+myCar[0][variable]= "Honda";
+
+console.log(myCar)
+
+
 
 let saleStatus = "forSale"
 myCar[saleStatus] = "Available"
 
 let fule = "fule"
 myCar[fule] = "CNG"
-console.log(myCar)
+// console.log(myCar)
 
-console.log(myCar["carName"], myCar["model"])
+// console.log(myCar["carName"], myCar["model"])
 
-console.log(myCar["forSale"])
+// console.log(myCar["forSale"])
 
 
 
-//
+//Objects in Ojects
+let company = {
+    companyName: "Easecad",
+    activity: ["Builders","Real Estate", "Hardware", "Material"],
+    address: {
+        city: "Quetta",
+        zipCode: 44778,
+        province: "Balochistan"
+    },
+    established: 2021
+}
+
+let activities = company.activity[1];
+console.log(activities)
+
+let megaCompanies = [{
+    burger: "kingBurger", 
+    pizza: "Dominos",
+    mamaHotel: "Cheese paratha"
+},
+
+45,
+{
+    dhaba: "chai",
+    tandoor: "roti",
+    filter: "pani",
+    bhook: true
+},
+"Biryani reh gayi bhai"
+]
+// console.log(megaCompanies[3])
+
+
+//==========> PRACTICE EXERCISE# 3.5 <==============//
+let people = {friends:[]}
+
+let friend1 = { 
+    firstName: "Ahsan",
+    lastName: "Khan",
+    id: 1
+}
+
+let friend2 = {
+    firstName: "Shahzoor",
+    lastName:"Khan",
+    id: 2
+}
+
+let friend3 = {
+    firstName: "Ammar",
+    lastName: "Ahmed",
+    id: 3
+}
+
+people.friends.push(friend1,friend2,friend3)
+console.log(people)
+
+//==============> CHAPTER#3 PROJECT<===============//
+
+const theList = ['Laurence', 'Svekis', true, 35, null, undefined,
+{test: 'one', score: 55}, ['one', 'two']];
+
+theList.pop()
+theList.shift()
+theList.unshift("FIRST")
+theList.splice(2,0,"hello world")
+theList.splice(2,0,"MIDDLE")
+theList.push("LAST")
+theList.splice(4,5)
+
+
+console.log(theList)
