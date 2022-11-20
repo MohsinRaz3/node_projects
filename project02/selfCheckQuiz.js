fe54bd8f876b04dadfdfdf63418087f6e12f52dc
mohsinraz3 <mohsinraz3@gmail.com>
2022-11-21 00:00:10 +0500

    selfcheckquiz chapter#2

---
+++ project02/selfCheckQuiz.js
@@ -0,0 +1,60 @@
+// String datatype
+const c = "5";
+
+
+// Number Data Type
+const d = 19;
+
+//Line 2 is better
+let empty1 = undefined; //line 1
+let empty2 = null; //line 2
+
+// world will be print 
+let a = "Hello";
+a = "world";
+console.log(a);
+
+//Hello world will be print
+let x = "world";
+let y = `Hello ${x}!`;
+console.log(y);
+
+//Hello will be printed
+let sen = "Hello";
+//sen = prompt("world");
+console.log(sen);
+
+// value of b will 71
+let aa = 5;
+let b = 70;
+let see = "5";
+b++;
+console.log(b);
+
+//4 will store in result
+let result = 3 + 4 * 2 / 8;
+console.log(result)
+
+//536 will answer
+let firstNum = 5;
+let secondNum = 10;
+firstNum++;
+secondNum--;
+let total = ++firstNum + secondNum;
+console.log(total);
+let total2 = 500 + 100 / 5 + total--;
+console.log(total2);
+
+//true
+//false
+//true
+//true
+//false 
+
+const ae = 5;
+const be = 10;
+console.log(ae > 0 && be > 0);
+console.log(ae == 5 && be == 4);
+console.log(true ||false);
+console.log(ae == 3 || be == 10);
+console.log(ae == 3 || be == 7);
